4eb749c37fc22f307212c0f9ce5b485121e27452
eritonld <eritonld@gmail.com>
2024-04-01 08:42:10 +0700

    migration_form_contents

---
+++ database/migrations/2024_03_28_072622_create_form_contents_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('form_contents', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('users_id')->unique();
+            $table->json('form_data')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('form_contents');
+    }
+};
