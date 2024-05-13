7671ba1144626d1d6e037c94f97ece3f3c16afa0
Alfian Azis <alfian.azis95@gmail.com>
2024-05-13 17:40:56 +0700

    add Roles module > create, manage, assign

--- resources/views/pages/roles/app.blade.php
+++ resources/views/pages/roles/app.blade.php
@@ -5,9 +5,9 @@
     <div class="container-fluid">
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-start mb-4">
-            <a class="btn btn-outline-primary rounded-pill btn-sm px-4 mr-3 {{ $active=='create' ? 'active':'' }}" href="{{ route('roles.create') }}">Create Role</a>
-            <a class="btn btn-outline-primary rounded-pill btn-sm px-4 mr-3 {{ $active=='manage' ? 'active':'' }}" href="{{ route('roles.manage') }}">Manage Role</a>
-            <a class="btn btn-outline-primary rounded-pill btn-sm px-4 mr-3 {{ $active=='assign' ? 'active':'' }}" href="{{ route('roles.assign') }}">Assign Users</a>
+            <a class="btn btn-outline-primary rounded-pill btn-sm px-4 mr-3 mb-2 {{ $active=='create' ? 'active':'' }}" href="{{ route('roles.create') }}">Create Role</a>
+            <a class="btn btn-outline-primary rounded-pill btn-sm px-4 mr-3 mb-2 {{ $active=='manage' ? 'active':'' }}" href="{{ route('roles.manage') }}">Manage Role</a>
+            <a class="btn btn-outline-primary rounded-pill btn-sm px-4 mr-3 mb-2 {{ $active=='assign' ? 'active':'' }}" href="{{ route('roles.assign') }}">Assign Users</a>
         </div>
         <!-- Content Row -->
         @if(session('success'))
