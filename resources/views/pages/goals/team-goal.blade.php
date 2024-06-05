1e0a8c903e1f1a5a0bcb8f5139a50e8e133fcf97
Alfian Azis <alfian.azis95@gmail.com>
2024-06-05 14:07:02 +0700

    update search in layer & schedule blade

--- resources/views/pages/goals/team-goal.blade.php
+++ resources/views/pages/goals/team-goal.blade.php
@@ -163,7 +163,7 @@
                                 <form id="exportNotInitiatedForm" action="{{ route('team-goals.notInitiated') }}" method="POST">
                                     @csrf
                                     <input type="hidden" name="employee_id" id="employee_id" value="{{ Auth()->user()->employee_id }}">
-                                    <button id="report-button" type="submit" class="btn btn-sm btn-outline-secondary rounded-pill float-end {{ isNotEmpty($notasks) ? '':'d-none' }}"><i class="ri-download-cloud-2-line me-1"></i><span>Report</span></button>
+                                    <button id="report-button" type="submit" class="btn btn-sm btn-outline-secondary rounded-pill float-end {{ $notasks ? '':'d-none' }}"><i class="ri-download-cloud-2-line me-1"></i><span>Report</span></button>
                                 </form>
                                 <!-- task -->
                                 @foreach ($notasks as $index => $notask)
