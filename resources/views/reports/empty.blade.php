5e2d0eb1fec9160ba20e7ad7385cad21dc2ab3fd
Alfian Azis <alfian.azis95@gmail.com>
2024-06-11 11:35:04 +0700

    add add preloader in report & onbehalfs

--- resources/views/reports/empty.blade.php
+++ resources/views/reports/empty.blade.php
@@ -2,7 +2,7 @@
     <div class="col-md-12">
     <div class="card shadow mb-4">
         <div class="card-body">
-            {{ __('No Report Found.') }}
+            {{ __('No Report Found. Please Select Report') }}
         </div>
     </div>
     </div>
