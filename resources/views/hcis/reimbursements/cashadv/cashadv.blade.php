8f3ed01b2d933337b5b69b4ecfa8c8e97d7b354e
Metta Saputra <its.mettasaputra@gmail.com>
2024-08-06 16:51:34 +0700

    beautify: fix layout form, add number format

--- resources/views/hcis/reimbursements/cashadv/cashadv.blade.php
+++ resources/views/hcis/reimbursements/cashadv/cashadv.blade.php
@@ -8,7 +8,7 @@
     <div class="container-fluid">
         <!-- Page Heading -->
         <div class="row">
-            <div class="col-12">
+            <div class="col-md-12">
                 <div class="page-title-box">
                     <div class="page-title-right">
                         <ol class="breadcrumb m-0">
@@ -23,14 +23,16 @@
 
         <div class="row">
             <div class="col-md-auto">
-              <div class="mb-3">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text bg-white border-dark-subtle"><i class="ri-search-line"></i></span>
-                  </div>
-                  <input type="text" name="customsearch" id="customsearch" class="form-control  border-dark-subtle border-left-0" placeholder="search.." aria-label="search" aria-describedby="search">
+                <div class="mb-3">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-white border-dark-subtle"><i class="ri-search-line"></i></span>
+                        </div>
+                        <input type="text" name="customsearch" id="customsearch"
+                            class="form-control  border-dark-subtle border-left-0" placeholder="search.."
+                            aria-label="search" aria-describedby="search">
+                    </div>
                 </div>
-              </div>
             </div>
             <div class="col">
                 <div class="mb-2 text-end">
@@ -40,80 +42,89 @@
         </div>
         <!-- Content Row -->
         <div class="row">
-          <div class="col-md-12">
-            <div class="card shadow mb-4">
-              <div class="card-body">
-                  <div class="table-responsive">
-                      <table class="table table-hover dt-responsive nowrap" id="scheduleTable" width="100%" cellspacing="0">
-                          <thead class="thead-light">
-                              <tr class="text-center">
-                                  <th>No</th>
-                                  <th>Type</th>
-                                  <th>No CA</th>
-                                  <th>Requestor</th>
-                                  <th>Company</th>
-                                  <th>Start Date</th>
-                                  <th>End Date</th>
-                                  <th>Total CA</th>
-                                  <th>Total Settlement</th>
-                                  <th>Balance</th>
-                                  <th>Actions</th>
-                              </tr>
-                          </thead>
-                          <tbody>
-                            @foreach($ca_transactions as $ca_transaction)
-                                <tr>
-                                    <td>{{ $loop->index + 1 }}</td>
-                                    @if($ca_transaction->type_ca == 'dns')
-                                        <td>Business Trip</td>
-                                    @elseif($ca_transaction->type_ca == 'ndns')
-                                        <td>Non Business Trip</td>
-                                    @elseif($ca_transaction->type_ca == 'entr')
-                                        <td>Entertainment</td>
-                                    @endif
-                                    <td>{{ $ca_transaction->no_ca }}</td>
-                                    <td>{{ $ca_transaction->employee->fullname }}</td>
-                                    <td>{{ $ca_transaction->contribution_level_code }}</td>
-                                    <td>{{ $ca_transaction->formatted_start_date }}</td>
-                                    <td>{{ $ca_transaction->formatted_end_date }}</td>
-                                    <td>{{ $ca_transaction->total_ca }}</td>
-                                    <td>{{ $ca_transaction->total_real }}</td>
-                                    <td>{{ $ca_transaction->total_cost }}</td>
-                                    <td class="text-center">
-                                        <div style="display: {{ $ca_transaction->approval_status == 'Approved' ? 'none' : 'inline-block' }}">
-                                            <a href="{{ route('cashadvanced.edit', $ca_transaction->id) }}" class="btn btn-sm rounded-pill btn-primary" title="Edit"><i class="ri-edit-box-line"></i></a>
-                                            <form action="{{ route('cashadvanced.delete', $ca_transaction->id) }}" method="POST" style="display:inline;">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button onclick="return confirm('Apakah ingin Menghapus?')" class="btn btn-sm rounded-pill btn-danger" title="Delete">
-                                                    <i class="ri-delete-bin-line"></i>
-                                                </button>
-                                            </form>
-                                        </div>
-                                        <div style="display: {{ $ca_transaction->approval_status == 'Approved' ? 'inline-block' : 'none' }}">
-                                            <a href="{{ route('cashadvanced.download', $ca_transaction->id) }}" target="_blank" class="btn btn-sm rounded-pill btn-primary" title="Print"><i class="ri-printer-line"></i></a>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                          </tbody>
-                      </table>
-                  </div>
-              </div>
+            <div class="col-md-12">
+                <div class="card shadow mb-4">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-hover dt-responsive nowrap" id="scheduleTable" width="100%"
+                                cellspacing="0">
+                                <thead class="thead-light">
+                                    <tr class="text-center">
+                                        <th>No</th>
+                                        <th>Type</th>
+                                        <th>Cash Advance No</th>
+                                        <th>Requestor</th>
+                                        <th>Company</th>
+                                        <th>Start Date</th>
+                                        <th>End Date</th>
+                                        <th>Total CA</th>
+                                        <th>Total Settlement</th>
+                                        <th>Balance</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($ca_transactions as $ca_transaction)
+                                        <tr>
+                                            <td class="text-center">{{ $loop->index + 1 }}</td>
+                                            @if ($ca_transaction->type_ca == 'dns')
+                                                <td>Business Trip</td>
+                                            @elseif($ca_transaction->type_ca == 'ndns')
+                                                <td>Non Business Trip</td>
+                                            @elseif($ca_transaction->type_ca == 'entr')
+                                                <td>Entertainment</td>
+                                            @endif
+                                            <td class="text-center">{{ $ca_transaction->no_ca }}</td>
+                                            <td>{{ $ca_transaction->employee->fullname }}</td>
+                                            <td>{{ $ca_transaction->contribution_level_code }}</td>
+                                            <td>{{ $ca_transaction->formatted_start_date }}</td>
+                                            <td>{{ $ca_transaction->formatted_end_date }}</td>
+                                            <td>Rp. {{ number_format($ca_transaction->total_ca) }}</td>
+                                            <td>Rp. {{ number_format($ca_transaction->total_real) }}</td>
+                                            <td>Rp. {{ number_format($ca_transaction->total_cost) }}</td>
+                                            <td class="text-center">
+                                                <div class=""
+                                                    style="display: {{ $ca_transaction->approval_status == 'Approved' ? 'none' : 'inline-block' }}">
+                                                    <a href="{{ route('cashadvanced.edit', $ca_transaction->id) }}"
+                                                        class="mx-1 btn btn-sm btn-outline-warning" title="Edit"><i
+                                                            class="ri-edit-box-line"></i></a>
+                                                    <form action="{{ route('cashadvanced.delete', $ca_transaction->id) }}"
+                                                        method="POST" style="display:inline;">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button onclick="return confirm('Apakah ingin Menghapus?')"
+                                                            class="btn btn-sm  btn-outline-danger" title="Delete">
+                                                            <i class="ri-delete-bin-line"></i>
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                                <div
+                                                    style="display: {{ $ca_transaction->approval_status == 'Approved' ? 'inline-block' : 'none' }}">
+                                                    <a href="{{ route('cashadvanced.download', $ca_transaction->id) }}"
+                                                        target="_blank" class="btn btn-sm rounded-pill btn-primary"
+                                                        title="Print"><i class="ri-printer-line"></i></a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-      </div>
+        </div>
     </div>
 @endsection
 
 @push('scripts')
-<script>
-    // Periksa apakah ada pesan sukses
-    var successMessage = "{{ session('success') }}";
+    <script>
+        // Periksa apakah ada pesan sukses
+        var successMessage = "{{ session('success') }}";
 
-    // Jika ada pesan sukses, tampilkan sebagai alert
-    if (successMessage) {
-        alert(successMessage);
-    }
-</script>
+        // Jika ada pesan sukses, tampilkan sebagai alert
+        if (successMessage) {
+            alert(successMessage);
+        }
+    </script>
 @endpush
