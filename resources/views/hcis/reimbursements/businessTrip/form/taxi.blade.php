0362f05a8d0714d8a4a0dd6b6f4b96252bcef6b4
Metta Saputra <its.mettasaputra@gmail.com>
2024-09-12 13:16:13 +0700

    update: Taxi Form

--- resources/views/hcis/reimbursements/businessTrip/form/taxi.blade.php
+++ resources/views/hcis/reimbursements/businessTrip/form/taxi.blade.php
@@ -6,34 +6,23 @@
         <div class="col-md-4 mb-2" id="taksi_div">
             <label class="form-label">Total Ticket</label>
             <div class="input-group input-group-sm">
-                <div class="input-group-append">
-                </div>
                 <input class="form-control" name="no_vt" id="no_vt" type="number" min="0"
                     placeholder="ex: 2">
             </div>
-            <div class="input-group input-group-sm mb-3">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
-              </div>
-              
         </div>
         <div class="col-md-4 mb-2">
             <label class="form-label">Voucher Nominal</label>
             <div class="input-group input-group-sm">
-                <div class="input-group-prepend">
                     <span class="input-group-text">Rp</span>
-                </div>
                 <input class="form-control" name="nominal_vt" id="nominal_vt" type="text" placeholder="ex. 12.000"
                     oninput="formatCurrency(this)">
             </div>
         </div>
         <div class="col-md-4 mb-2">
             <label class="form-label">Voucher Keeper</label>
-            <div class="input-group">
-                <div class="input-group-prepend">
+            <div class="input-group input-group-sm">
                     <span class="input-group-text">Rp</span>
-                </div>
-                <input class="form-control form-control-sm" name="keeper_vt" id="keeper_vt" type="text" placeholder="ex. 12.000"
+                <input class="form-control" name="keeper_vt" id="keeper_vt" type="text" placeholder="ex. 12.000"
                     oninput="formatCurrency(this)">
             </div>
         </div>
