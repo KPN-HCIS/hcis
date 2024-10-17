25f87c319bea0c9a9909922f2e68293b97034c59
Metta Saputra <its.mettasaputra@gmail.com>
2024-10-17 11:39:08 +0700

    add: Nav Pills

--- resources/views/hcis/reimbursements/medical/medical.blade.php
+++ resources/views/hcis/reimbursements/medical/medical.blade.php
@@ -59,7 +59,7 @@
                     </li>
                 </ol>
             </div>
-        
+
             <!-- Button Section -->
             <div class="col-md-6 d-flex justify-content-center justify-content-md-end align-items-center">
                 <a href="{{ route('export.excel') }}" class="btn btn-outline-success rounded-pill btn-action me-1">
@@ -70,8 +70,34 @@
                 </a>
             </div>
         </div>
-        
-        
+
+        <div class="row">
+            <div class="card shadow-none p-1 py-3 d-flex align-items-center">
+                        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home"
+                                    type="button" role="tab" aria-controls="pills-home" aria-selected="true">History</button>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile"
+                                    type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Plafon
+                                    Medical</button>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact"
+                                    type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Family
+                                    Data</button>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="pills-tabContent">
+                            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...
+                            </div>
+                            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
+                            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+                        </div>
+            </div>
+        </div>
+
         <div class="row mt-2">
             {{-- Data Keluarga --}}
             <div class="card shadow-none">
