0566a8b029786d2f77e0f4539384331fde3ac3f2
Erzie Aldrian <erzie.aldrian02@gmail.com>
2024-10-25 13:47:36 +0700

    sesuai dept head div head di excel

--- app/Exports/CashAdvancedExport.php
+++ app/Exports/CashAdvancedExport.php
@@ -4,6 +4,8 @@ namespace App\Exports;
 
 use App\Models\CATransaction;
 use App\Models\Employee;
+use App\Models\ca_approval;
+use App\Models\ca_sett_approval;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
@@ -33,7 +35,6 @@ class CashAdvancedExport implements FromCollection, WithHeadings, WithStyles, Wi
     public function collection()
     {
         $query = CATransaction::select(
-
             DB::raw("CASE
                 WHEN ca_transactions.type_ca = 'dns' THEN 'Dinas'
                 WHEN ca_transactions.type_ca = 'ndns' THEN 'Non Dinas'
@@ -49,8 +50,30 @@ class CashAdvancedExport implements FromCollection, WithHeadings, WithStyles, Wi
             'ca_transactions.contribution_level_code',
             'employees.employee_id',
             'employees.fullname',
-            DB::raw("manager1.fullname as manager_l1_fullname"),
-            DB::raw("manager2.fullname as manager_l2_fullname"),
+            DB::raw("(
+                    SELECT GROUP_CONCAT(DISTINCT e1.fullname ORDER BY layer ASC)
+                    FROM ca_approvals ca1
+                    LEFT JOIN employees e1 ON FIND_IN_SET(e1.employee_id, (
+                        SELECT GROUP_CONCAT(DISTINCT employee_id ORDER BY layer ASC)
+                        FROM ca_approvals
+                        WHERE ca_approvals.ca_id = ca_transactions.id
+                        AND role_name = 'Dept Head'
+                    )) > 0
+                    WHERE ca1.ca_id = ca_transactions.id
+                    AND ca1.role_name = 'Dept Head'
+                ) AS manager_l1_fullnames"),
+            DB::raw("(
+                    SELECT GROUP_CONCAT(DISTINCT e2.fullname ORDER BY layer ASC)
+                    FROM ca_approvals ca2
+                    LEFT JOIN employees e2 ON FIND_IN_SET(e2.employee_id, (
+                        SELECT GROUP_CONCAT(DISTINCT employee_id ORDER BY layer ASC)
+                        FROM ca_approvals
+                        WHERE ca_approvals.ca_id = ca_transactions.id
+                        AND role_name = 'Div Head'
+                    )) > 0
+                    WHERE ca2.ca_id = ca_transactions.id
+                    AND ca2.role_name = 'Div Head'
+                ) AS manager_l2_fullnames"),
             'ca_transactions.no_ca',
             'ca_transactions.no_sppd',
             'ca_transactions.total_ca',
@@ -61,13 +84,13 @@ class CashAdvancedExport implements FromCollection, WithHeadings, WithStyles, Wi
             'ca_transactions.approval_extend',
             DB::raw("DATEDIFF(CURDATE(), ca_transactions.declare_estimate) as days_difference"),
             DB::raw("CASE
-                        WHEN DATEDIFF(CURDATE(), ca_transactions.declare_estimate) > 0 THEN 'Overdue'
-                        ELSE 'Not Overdue'
-                    END as overdue_status"),
+                WHEN DATEDIFF(CURDATE(), ca_transactions.declare_estimate) > 0 THEN 'Overdue'
+                ELSE 'Not Overdue'
+            END as overdue_status"),
             DB::raw("CASE
-                    WHEN DATEDIFF(CURDATE(), ca_transactions.declare_estimate) > 0 THEN ca_transactions.total_ca
-                    ELSE 0
-                END as total_ca_adjusted"),
+                WHEN DATEDIFF(CURDATE(), ca_transactions.declare_estimate) > 0 THEN ca_transactions.total_ca
+                ELSE 0
+            END as total_ca_adjusted"),
             DB::raw("CASE
                 WHEN DATEDIFF(CURDATE(), ca_transactions.declare_estimate) BETWEEN 0 AND 6 THEN ca_transactions.total_ca
                 ELSE 0
@@ -83,11 +106,12 @@ class CashAdvancedExport implements FromCollection, WithHeadings, WithStyles, Wi
             DB::raw("CASE
                 WHEN DATEDIFF(CURDATE(), ca_transactions.declare_estimate) BETWEEN 30 AND 999 THEN ca_transactions.total_ca
                 ELSE 0
-            END as total_ca_within_99_days"),
+            END as total_ca_within_99_days")
         )
-            ->leftJoin('employees', 'ca_transactions.user_id', '=', 'employees.id')
+            ->join('employees', 'ca_transactions.user_id', '=', 'employees.id')
             ->leftJoin('employees as manager1', 'employees.manager_l1_id', '=', 'manager1.employee_id')
-            ->leftJoin('employees as manager2', 'employees.manager_l2_id', '=', 'manager2.employee_id'); // lakukan left join dengan tabel employee
+            ->leftJoin('employees as manager2', 'employees.manager_l2_id', '=', 'manager2.employee_id')
+            ->groupBy('ca_transactions.id');
 
         if ($this->startDate && $this->endDate) {
             $query->whereBetween('ca_transactions.start_date', [$this->startDate, $this->endDate]);
--- app/Models/CATransaction.php
+++ app/Models/CATransaction.php
@@ -11,7 +11,28 @@ class CATransaction extends Model
 {
     use HasFactory, SoftDeletes;
     protected $fillable = [
-        'id','type_ca','no_ca','no_sppd','user_id','unit','contribution_level_code','destination','ca_needs','start_date','end_date','date_required','detail_ca','total_ca','total_real','total_cost','approval_status','approval_sett','approval_extend','total_days', 'created_by', 'caonly'
+        'id',
+        'type_ca',
+        'no_ca',
+        'no_sppd',
+        'user_id',
+        'unit',
+        'contribution_level_code',
+        'destination',
+        'ca_needs',
+        'start_date',
+        'end_date',
+        'date_required',
+        'detail_ca',
+        'total_ca',
+        'total_real',
+        'total_cost',
+        'approval_status',
+        'approval_sett',
+        'approval_extend',
+        'total_days',
+        'created_by',
+        'caonly'
     ];
     protected $table = 'ca_transactions';
 
@@ -50,7 +71,7 @@ class CATransaction extends Model
     }
     public function approvals()
     {
-        return $this->hasMany(ca_approval::class, 'id', 'id');
+        return $this->hasMany(ca_approval::class, 'id', 'ca_id');
     }
     public function statusReqEmployee()
     {
